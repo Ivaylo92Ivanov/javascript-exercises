6e0520fbbb2a078901ccc6c20161470c62f63f6f
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-02-26 11:12:20 +0200

    Add solutions to ex1, ex2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text, multiplier) {
+    let resultText = "";
+    if (multiplier < 0) {
+        return "ERROR";
+    } else if (multiplier === 0) {
+        return resultText;
+    };
+    for (let i = 0; i < multiplier; i++) {
+        resultText += text;
+    };
+    return resultText
 };
 
 // Do not edit below this line
