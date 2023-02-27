3240dbb8cd3083fe1b5aaabb2bf96761e8f40b12
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-02-27 14:51:12 +0200

    Add solution to ex.7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesFahrenheit) {
+  let degreesCelsius = (degreesFahrenheit - 32) * 5 / 9;
+  return parseFloat(degreesCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesCelsius) {
+  let degreesFahrenheit = (degreesCelsius * 9 / 5) + 32;
+  return parseFloat(degreesFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
