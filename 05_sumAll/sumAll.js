2543eafe783a9ba8dbd36e09f993ed53b6e90347
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-02-27 13:41:55 +0200

    Add solution to ex5.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let resultSum = 0;
+    //check if arguments are negative numbers
+    if ((num1 <0 || num2 <0)) {
+        return "ERROR";
+    };
+    //check if arguments are not numbers
+    if (typeof(num1) !== "number" || typeof(num2) !== "number") {
+        return "ERROR";
+    };
+    //if num1 is larger, switch places of num1 and num2, so for cycle works properly
+    if (num1 > num2) {
+        [num1, num2] = [num2, num1]
+    };
+    for (num1; num1 <= num2; num1++) {
+        resultSum += num1;
+    };
+    return resultSum;
 };
 
 // Do not edit below this line
