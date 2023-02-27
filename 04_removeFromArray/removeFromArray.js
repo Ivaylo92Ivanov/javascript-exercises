6a6a7414d8cd26d80673e9feda97b5b7a03224e0
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-02-27 09:28:22 +0200

    Add solution to ex4.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, ...args) {
+    const resultArray = []; 
+    const itemsToRemove = [...args];
+    for (const item of list) {
+        if (itemsToRemove.includes(item)) {
+            continue;
+        };
+        resultArray.push(item);
+    };
+    return resultArray
+    
 };
 
 // Do not edit below this line
