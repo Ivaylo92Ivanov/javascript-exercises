4a7663b66144d065f776bd9ad7f6f23c8e8876fb
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-03-18 11:14:57 +0200

    Add solution to ex 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
+const add = function(a, b) {
+	return a+b
+};
+
+const subtract = function(a, b) {
+	return a-b
+};
+
+const sum = function(a) {
+	let result = 0;
+  a.forEach(num => result += num)
+  return result
+};
+
+const multiply = function(a) {
+  let result = 1;
+  a.forEach(num => result *= num)
+  return result
+};
+
+const power = function(a, b) {
+  return a**b
 	
 };
 
-const subtract = function() {
-	
+const factorial = function(a) {
+  result = 1
+	if (a ===0) return result
+  for (a; a > 0; a--) {
+    result *= a
+  }
+  return result
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorials = function (n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    console.log(i)
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
