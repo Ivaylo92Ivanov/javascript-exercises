091030b368482a39256226a2884dd94d0b0edb61
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-03-18 12:43:43 +0200

    Add solution to ex.9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    
+    let wordReversed = "";
+    let wordReversedAsArray = (word.split('').reverse());
+    wordReversedAsArray.forEach(letter => letter.toLowerCase() !== letter.toUpperCase() ? wordReversed += letter : 0)
 
+    let wordTruncated = ""
+    let wordTruncatedAsArray = word.split("")
+    wordTruncatedAsArray.forEach(letter => letter.toLowerCase() !== letter.toUpperCase() ? wordTruncated += letter : 0)
+
+    return wordReversed.toLowerCase() === wordTruncated.toLowerCase()
 };
 
 // Do not edit below this line
