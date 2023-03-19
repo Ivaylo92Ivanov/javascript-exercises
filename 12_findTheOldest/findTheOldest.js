d4db4dbaf9538add35da465d3e20a91729dd406a
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-03-19 10:39:28 +0200

    Add solution to ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    // In case yearOfDeath is missing, adding current date as yearOfDeath, so there can be a calculation of current age of a living person
+    const calculationList = [];
+    people.forEach(person => {
+        if (!person.hasOwnProperty("yearOfDeath")) {
+            person["yearOfDeath"] = (new Date()).getFullYear();
+        }
+        calculationList.push(person);        
+    });
+
+    const sortedPeople = calculationList.sort(function(personA, personB) {
+        if (personA.yearOfDeath - personA.yearOfBirth > personB.yearOfDeath - personB.yearOfBirth) {
+            return -1;
+        } else {
+            return 1;
+        };
+    });
+
+    return sortedPeople[0];
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
