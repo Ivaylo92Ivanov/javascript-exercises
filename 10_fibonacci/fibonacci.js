6bf22433ea254e1676c17c2fe3500cdc5fae1175
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-03-19 09:50:36 +0200

    Add solution to ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num <0) return "OOPS"
+    let firstNum = 1;
+    let nextNum = 1;
+    let resultNum = 0;
+    for (let i = 0; i <num; i++) {
+        resultNum = firstNum + nextNum;
+        firstNum = nextNum;
+        nextNum = resultNum;
+    };
+    return resultNum-firstNum
+    };
 
 // Do not edit below this line
 module.exports = fibonacci;
